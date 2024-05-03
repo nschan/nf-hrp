73c76db400557dd4723d02a70df71ee10b598ddc
Niklas Schandry <niklas@bio.lmu.de>
2024-05-03 14:42:19 +0200

    update filter_R_genes.R

--- bin/filter_R_genes.R
+++ bin/filter_R_genes.R
@@ -10,13 +10,46 @@ args <- commandArgs(trailingOnly = TRUE)
 
 # Args are:
 # file1 (e.g. Pfam)
-# file2 (e.g. Superfamily)
+# file2 (e.g. Superfam)
+# more files
 # outfile (Basename only)
 
 # Define domain - pfam or superfam relationships
 ## Currently, these use what is defined by HRP
 
-domains <-
+domains_interpro <-
+  data.frame(
+    Class = c(
+      "C",
+      "T",
+      "T",
+      "R",
+      "L",
+      "L",
+      "L",
+      "L",
+      "L",
+      "L",
+      "L",
+      "N"
+    ),
+    Family = c(
+      "IPR041118",
+      "IPR000157",
+      "IPR035897",
+      "IPR044974",
+      "IPR001611",
+      "IPR003591",
+      "IPR006553",
+      "IPR011713",
+      "IPR013101",
+      "IPR013210",
+      "IPR032675",
+      "IPR002182"
+    )
+  )
+
+domains_pfam <-
   data.frame(
     Class = c(
       "C",
@@ -56,18 +89,22 @@ domains <-
 
 # Subset into individual classes
 
-TIR <-  filter(domains, Class == "T")
-COIL <- filter(domains, Class == "C")
-RPW8 <- filter(domains, Class == "R")
-LRR <-  filter(domains, Class == "L")
-NB <-  filter(domains, Class == "N")
+TIR_interpro <-  filter(domains_interpro, Class == "T")
+COIL_interpro <- filter(domains_interpro, Class == "C")
+RPW8_interpro <- filter(domains_interpro, Class == "R")
+LRR_interpro <-  filter(domains_interpro, Class == "L")
+NB_interpro <-  filter(domains_interpro, Class == "N")
+TIR_pfam <-  filter(domains_pfam, Class == "T")
+COIL_pfam <- filter(domains_pfam, Class == "C")
+RPW8_pfam <- filter(domains_pfam, Class == "R")
+LRR_pfam <-  filter(domains_pfam, Class == "L")
+NB_pfam <-  filter(domains_pfam, Class == "N")
 
 # Read in files
 
-rbind(
-  read_tsv(args[1], col_names = F, show_col_types = FALSE),
-  read_tsv(args[2], col_names = F, show_col_types = FALSE)
-) %>%
+lapply(args[1:(length(args)-1)],
+       \(x) read_tsv(x, col_names = F, show_col_types = FALSE)) %>%
+  bind_rows() %>%
   magrittr::set_colnames(
     c(
       "ID",
@@ -88,26 +125,36 @@ rbind(
     )
   ) %>% 
   # Check TIR, COIL, RPW, NB and LRR presence
-  mutate(has_TIR = case_when(Family %in% TIR$Family ~ TRUE,
+  mutate(has_TIR = case_when(InterPro_Accession %in% TIR_interpro$Family ~ TRUE,
+                             Family %in% TIR_pfam$Family ~ TRUE,
                              TRUE ~ FALSE),
-         has_TIR_evidence = case_when(Family %in% TIR$Family ~ Family,
-                             TRUE ~ NA_character_),
-         has_COIL = case_when(Family %in% COIL$Family ~ TRUE,
+         has_TIR_evidence = case_when(InterPro_Accession %in% TIR_pfam$Family ~ InterPro_Accession,
+                                      Family %in% TIR_pfam$Family ~ Family,
+                                      TRUE ~ NA_character_),
+         has_COIL = case_when(InterPro_Accession %in% COIL_interpro$Family ~ TRUE,
+                              Family %in% COIL_pfam$Family ~ TRUE,
                               TRUE ~ FALSE),
-         has_COIL_evidence = case_when(Family %in% COIL$Family ~ Family,
-                             TRUE ~ NA_character_),
-         has_RPW8 = case_when(Family %in% RPW8$Family ~ TRUE,
+         has_COIL_evidence = case_when(InterPro_Accession %in% COIL_interpro$Family ~ InterPro_Accession,
+                                       Family %in% COIL_pfam$Family ~ Family,
+                                       TRUE ~ NA_character_),
+         has_RPW8 = case_when(InterPro_Accession %in% RPW8_interpro$Family ~ TRUE,
+                              Family %in% RPW8_pfam$Family ~ TRUE,
                               TRUE ~ FALSE),
-         has_RPW8_evidence = case_when(Family %in% RPW8$Family ~ Family,
-                              TRUE ~ NA_character_),
-         has_NB = case_when(Family %in% NB$Family ~ TRUE,
-                              TRUE ~ FALSE),
-         has_NB_evidence = case_when(Family %in% NB$Family ~ Family,
-                              TRUE ~ NA_character_),
-         has_LRR = case_when(Family %in% LRR$Family ~ TRUE,
-                              TRUE ~ FALSE),
-         has_LRR_evidence = case_when(Family %in% LRR$Family ~ Family,
-                              TRUE ~ NA_character_)) %>% 
+         has_RPW8_evidence = case_when(InterPro_Accession %in% RPW8_interpro$Family ~ InterPro_Accession,
+                                       Family %in% RPW8_pfam$Family ~ Family,
+                                       TRUE ~ NA_character_),
+         has_NB = case_when(InterPro_Accession %in% NB_interpro$Family ~ TRUE,
+                            Family %in% NB_pfam$Family ~ TRUE,
+                            TRUE ~ FALSE),
+         has_NB_evidence = case_when(InterPro_Accession %in% NB_interpro$Family ~ InterPro_Accession,
+                                     Family %in% NB_pfam$Family ~ Family,
+                                     TRUE ~ NA_character_),
+         has_LRR = case_when(InterPro_Accession %in% LRR_interpro$Family ~ TRUE,
+                             Family %in% LRR_pfam$Family ~ TRUE,
+                             TRUE ~ FALSE),
+         has_LRR_evidence = case_when(InterPro_Accession %in% LRR_interpro$Family ~ InterPro_Accession,
+                                      Family %in% LRR_pfam$Family ~ Family,
+                                      TRUE ~ NA_character_)) %>% 
   # Make uniques
   group_by(ID) %>% 
   summarize(has_TIR = case_when(sum(has_TIR)  > 0 ~ TRUE, TRUE ~ FALSE),
@@ -122,18 +169,18 @@ rbind(
             LRR_evidence = str_flatten(has_LRR_evidence %>% {.[!is.na(.)]}, collapse = " ")
             ) %>% 
   # Filter of NB-LRR
-  filter(has_NB & has_LRR) %>% 
+  filter(has_RPW8 | (has_NB & has_LRR )) %>% 
   # Annotate
   mutate(Type = case_when(
     has_TIR ~ "TNL",
     has_COIL ~ "CNL",
-    has_RPW8 ~ "RNL",
+    has_RPW8 ~ "NLR",
     TRUE ~ "NL"
   )) %>% 
   ungroup() %T>%
   # Write larger table
-  write_tsv(paste0(args[3],"_NLR_table.tsv")) %>% 
+  write_tsv(paste0(args[length(args)],"_NLR_table.tsv")) %>% 
   dplyr::select(ID, Type) %>% 
   # Write relevant table
-  write_tsv(paste0(args[3],"_NLR_genes.tsv"))
+  write_tsv(paste0(args[length(args)],"_NLR_genes.tsv"))
          
